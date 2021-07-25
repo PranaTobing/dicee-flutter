c1c28b83aa184e8501ff89656297ffe2394bd39e
pranatobing21 <prana.tobing@tech.jago.com>
2021-07-25 18:14:15 +0700

    final dicee project

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,10 +6,10 @@ void main() {
     MaterialApp(
       title: 'dicee app',
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.amber,
         appBar: AppBar(
           title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.amber,
         ),
         body: DicePage(),
       ),
@@ -16,9 +17,42 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key key}) : super(key: key);
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int daduKiri = 4;
+  int daduKanan = 1;
+  void _changeDice() {
+    setState(() {
+      daduKiri = Random().nextInt(6) + 1;
+      daduKanan = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: _changeDice,
+              child: Image.asset('images/dice$daduKiri.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: _changeDice,
+              child: Image.asset('images/dice$daduKanan.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
